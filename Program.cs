49c082bda8505cae340b592da6039020a1bc1df6
DenisBir <124333344+DenisBir@users.noreply.github.com>
2023-02-11 16:51:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Ведите числа для сравнения");
+Console.Write("Ведите число А - ");
+int numberA = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Ведите число В - ");
+int numberB = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Ведите число C - ");
+int numberC = int.Parse(Console.ReadLine() ?? "0");
+
+int max = numberA;
+
+if (numberA > max ) max = numberA;
+if (numberB > max ) max = numberB;
+if (numberC > max ) max = numberC;
+
+Console.Write("max = ");
+
+Console.WriteLine(max);
